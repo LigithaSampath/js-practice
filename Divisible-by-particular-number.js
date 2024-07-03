dc37146bc033ba72385d98a60e0a1542eda48126
Sampath <lsampath@ad.esi.adp.com>
2024-07-03 15:39:03 +0100

    number is divisilbe by a particular or other number

---
+++ Divisible-by-particular-number.js
@@ -0,0 +1,15 @@
+//check given number is divisilbe by a particular other number using ternary operator 
+let num = 25;
+let result = (num % 2 == 0) ? "true" : "fasle"
+console.log(result);
+
+// check given number is divisible by a particular orther number using function methods
+function divisible(num) {
+  if (num % 3 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+let res = divisible(3)
+console.log(res)
